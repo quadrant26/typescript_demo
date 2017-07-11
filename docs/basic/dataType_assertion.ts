aa8847b5c4594a2b377cdac6b094bcbf14f3386f
quadrant26 <quadrant26@163.com>
2017-07-11 15:59:30 +0800

    typescript 类型断言

---
+++ docs/basic/dataType_assertion.ts
@@ -0,0 +1,9 @@
+// 类型断言
+
+function getLength(something: string | number): number {
+    if ((<string>something).length) {
+        return (<string>something).length;
+    } else {
+        return something.toString().length;
+    }
+}
