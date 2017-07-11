01a8374978d00b958278d0a9033d3902b666ef6e
quadrant26 <quadrant26@163.com>
2017-07-11 17:54:24 +0800

    typescript 泛型

---
+++ docs/advanced/advanced_generics.ts
@@ -0,0 +1,21 @@
+// 泛型
+
+function createArray<T>(length: number, value: T): Array<T> {
+    let result = [];
+    for (let i = 0; i < length; i++) {
+        result[i] = value;
+    }
+    return result;
+}
+
+createArray<string>(3, 'x'); // ['x', 'x', 'x']
+
+// 泛型约束
+interface Lengthwise {
+  length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
